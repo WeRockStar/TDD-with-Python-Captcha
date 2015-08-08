c548d8b8979f59c5a7b154648a0e8d40e2315983
comfreestyle <vr_freestyle@outlook.com>
2015-08-08 10:27:22 +0700

    Add 2 == 2

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,9 +1,15 @@
 import unittest
 class FizzBuzz():
     def count(self , number):
-        return 1
+        if number == 1:
+            return '1'
+        return '2'
 
 class TestFizzBuzz(unittest.TestCase):
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
     def test_it_should_return_1_when_is_1(self):
-        fizzbuzz = FizzBuzz()
-        self.assertEqual(1 , fizzbuzz.count(1))
+        self.assertEqual('1' , fizzbuzz.count(1))
+    def test_it_should_return_2_when_is_2(self):
+        self.assertEqual('2' , fizzbuzz.count(2))
