9c22f33e1c2606c07edd51f00d1c3b627db3fa6e
comfreestyle <vr_freestyle@outlook.com>
2015-08-08 15:00:41 +0700

    Add Test Operator Number

--- test_operand.py
+++ test_operand.py
@@ -9,3 +9,12 @@ class TestOperandString(unittest.TestCase):
     def test_2_should_Two(self):
         operandStr = OperandString(2)
         self.assertEqual('Two' , operandStr.toString())
+
+class TestOperandNumber(unittest.TestCase):
+    def test_1_should_2(self):
+        operandNum = OperandNumber(1)
+        self.assertEqual(1 , operandNum.toString())
+
+    def test_2_should_2(self):
+        operandNum = OperandNumber(2)
+        self.assertEqual(2 , operandNum.toString())
