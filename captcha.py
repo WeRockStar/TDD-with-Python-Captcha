73bb28e0831564f50b4a19d058cd5d91655099e8
comfreestyle <vr_freestyle@outlook.com>
2015-08-08 13:52:07 +0700

    Test Second Left

--- captcha.py
+++ captcha.py
@@ -1,6 +1,23 @@
 class Captcha():
+    pattern = 0
     def __init__(self , pattern ,left , operator , right):
         self.pattern = pattern
         self.left = left
         self.operator = operator
         self.right = right
+
+    def leftOperator(self):
+        return str(self.left)
+
+
+class SecordPatternOperland():
+    left = 0
+    patternL = {'1' : 'One' , '2' : 'Two'}
+    def __init__(self , pattern ,left , operator , right):
+        self.pattern = pattern
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def leftOperator(self):
+        return self.patternL[str(self.left)]
--- test_captcha.py
+++ test_captcha.py
@@ -1,7 +1,31 @@
 import unittest
-from captcha import Captcha
+from captcha import Captcha,SecordPatternOperland
 
-class TestFirstPatternLeftOperland(unittest.TestCase):
+class TestFirstPatternLeftOperand(unittest.TestCase):
+    def setUp(self):
+        self.first_pattern = 1
+        self.left = 1
+        self.op = 1
+        self.rigth = 1
+
     def test_1_should_1(self):
-        captcha = Captcha(1,1,1,1)
-        self.assertEqual(1 , captcha.left)
+        captcha = Captcha(self.first_pattern , 1 , self.op , self.rigth)
+        self.assertEqual('1' , captcha.leftOperator())
+
+    def test_2_should_2(self):
+        captcha = Captcha(self.first_pattern , 2 , self.op , self.rigth)
+        self.assertEqual('2' , captcha.leftOperator())
+
+class TestSecondPatternLeftOperand(unittest.TestCase):
+    def setUp(self):
+        self.second_pattern = 2
+        self.op = 1
+        self.rigth = 1
+
+    def test_1_should_One(self):
+        leftCaptcha = SecordPatternOperland(self.second_pattern , 1 , self.op , self.rigth)
+        self.assertEqual('One' , leftCaptcha.leftOperator())
+
+    def test_2_should_Two(self):
+        leftCaptcha = SecordPatternOperland(self.second_pattern , 2 , self.op , self.rigth)
+        self.assertEqual('Two' , leftCaptcha.leftOperator())
