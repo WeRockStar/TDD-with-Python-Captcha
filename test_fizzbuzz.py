3636e54571d723ebf3c04beb73aa00370b99a802
comfreestyle <vr_freestyle@outlook.com>
2015-08-08 10:42:23 +0700

    Add 3 == Fizz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,9 +1,9 @@
 import unittest
 class FizzBuzz():
     def count(self , number):
-        if number == 1:
-            return '1'
-        return '2'
+        if number == 3:
+            return 'Fizz'
+        return str(number)
 
 class TestFizzBuzz(unittest.TestCase):
     def setUp(self):
@@ -11,5 +11,9 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_it_should_return_1_when_is_1(self):
         self.assertEqual('1' , self.fizzbuzz.count(1))
+
     def test_it_should_return_2_when_is_2(self):
         self.assertEqual('2' , self.fizzbuzz.count(2))
+
+    def test_it_should_return_fizz_when_is_3(self):
+        self.assertEqual('Fizz' , self.fizzbuzz.count(3))
